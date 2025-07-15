75e64754dab19bc96b33cf3e29a674400526b1f1
chugue <chugue85@gmail.com>
2025-07-15 22:33:21 +0900

    save: 0715 06:38

---
+++ components/stat-card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface StatCardProps {
+  type: "appointments" | "pending" | "cancelled";
+  count: number;
+  label: string;
+  icon: string;
+}
+
+const StatCard = ({ count = 0, label, icon, type }: StatCardProps) => {
+  return <div>StatCard</div>;
+};
+
+export default StatCard;
